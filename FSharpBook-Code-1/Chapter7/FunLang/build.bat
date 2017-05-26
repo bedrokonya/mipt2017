db2f10ed6d7ca0ce69c3d939135040be7e31865f
dmitrieva irina <atlantie@yandex.ru>
2017-05-26 12:52:58 +0300

    deleted excess materials

--- FSharpBook-Code-1/Chapter7/FunLang/build.bat
+++
@@ -1,3 +0,0 @@
-call fslex -o lex.fs --unicode lex.fsl
-call fsyacc -o pars.fs --module Pars pars.fsy
-call fsc -g -o interp.exe -r "FSharp.Powerpack.dll" ast.fs pars.fsi pars.fs lex.fs lambdaint.fs main.fs
